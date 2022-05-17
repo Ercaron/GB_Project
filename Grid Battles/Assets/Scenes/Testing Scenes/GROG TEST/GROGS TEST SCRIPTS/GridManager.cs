c3cd48838da255ba5b930134573d7435f72e1e1f
Gregorio <gregogape@gmail.com>
2022-05-17 02:13:23 -0300

    Different Grid Logic - added stuff
    hago el push asi re sidoso xq nose si siquiera se va a usar esto. creo q quedo nice igual

--- Grid Battles/Assets/Scenes/Testing Scenes/GROG TEST/GROGS TEST SCRIPTS/GridManager.cs
+++ Grid Battles/Assets/Scenes/Testing Scenes/GROG TEST/GROGS TEST SCRIPTS/GridManager.cs
@@ -11,24 +11,34 @@ public class GridManager : MonoBehaviour
 
     [SerializeField] Transform _mainCameraTransform;
 
-
+    private Dictionary<Vector2, GridTile> _tiles;
     private void Start()
     {
         GenerateGrid();
     }
     void GenerateGrid()
     {
+        _tiles = new Dictionary<Vector2, GridTile>();
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
             {
                 GridTile spawnedTile = Instantiate(_gridTilePrefab, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"GridTile {x} {y}";
+
+                _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
 
         _mainCameraTransform.position = new Vector3(_width / 2, _height / 2, -10);
     }
 
-    
+    public GridTile getTileAtPosition(Vector2 position)
+    {
+        if(_tiles.TryGetValue(position,out GridTile tile))
+        {
+            return tile;
+        }
+        return null;
+    }
 }
