6e7ca4f7df1185c79e1e68f19eab46bf307e74ec
Gregorio <gregogape@gmail.com>
2022-05-20 02:10:00 -0300

    Name changes, folder changes, ETC

--- Grid Battles/Assets/Scripts/GridManager.cs
+++ Grid Battles/Assets/Scripts/GridManager.cs
@@ -7,23 +7,23 @@ public class GridManager : MonoBehaviour
     [SerializeField] int _height;
     [SerializeField] int _width;
 
-    [SerializeField] private GridTile _gridTilePrefab;
+    [SerializeField] private Tile _gridTilePrefab;
 
     [SerializeField] Transform _mainCameraTransform;
 
-    private Dictionary<Vector2, GridTile> _tiles;
+    private Dictionary<Vector2, Tile> _tiles;
     private void Start()
     {
         GenerateGrid();
     }
     void GenerateGrid()
     {
-        _tiles = new Dictionary<Vector2, GridTile>();
+        _tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
             {
-                GridTile spawnedTile = Instantiate(_gridTilePrefab, new Vector3(x, y), Quaternion.identity);
+                Tile spawnedTile = Instantiate(_gridTilePrefab, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"GridTile {x} {y}";
 
                 _tiles[new Vector2(x, y)] = spawnedTile;
@@ -33,9 +33,11 @@ public class GridManager : MonoBehaviour
         _mainCameraTransform.position = new Vector3(_width / 2, _height / 2, -10);
     }
 
-    public GridTile getTileAtPosition(Vector2 position)
+
+
+    public Tile getTileAtPosition(Vector2 position)
     {
-        if(_tiles.TryGetValue(position,out GridTile tile))
+        if(_tiles.TryGetValue(position,out Tile tile))
         {
             return tile;
         }
