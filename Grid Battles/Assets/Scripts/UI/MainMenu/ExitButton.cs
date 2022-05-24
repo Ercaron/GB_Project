961ecacd590083bcf4b9713fd86b62c3f156ab85
Gregorio <gregogape@gmail.com>
2022-05-24 03:52:47 -0300

    MainMenu updates

---
+++ Grid Battles/Assets/Scripts/UI/MainMenu/ExitButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ExitButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnButtonClick()
+    {
+
+    }
+}
